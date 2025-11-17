aa5342021940cd89caf8a8ac6f390641f9788ee8
vikas choudhary <vikaschoudhary@vikass-MacBook-Air.local>
2025-11-17 08:19:37 +0530

    push all code

--- server.js
+++ server.js
@@ -85,6 +85,9 @@ app.use("/api/login", loginRoutes);
 
 // SERVER RUN
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`🚀 Server running on port ${PORT}`);
+// });
+
+
+app.listen(PORT, "0.0.0.0", () => console.log(`🚀 Server running on port ${PORT}`));
